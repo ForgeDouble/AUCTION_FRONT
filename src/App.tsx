4e69c6c316180d964abc512340a0dff74606610c
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-07-31 20:40:36 +0900

    chore:  tailwind css 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,8 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="text-3xl font-bold text-green-800 p-4 bg-white">
+        Tailwind CSS 작동 중!
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
