71ff02b707d77a6dd5c6e6b4e27549c8641cec22
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-09-13 22:33:33 +0900

    Chore: global 변수 찾도록 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    global: "window", // global → window로 치환
+  },
   plugins: [react()],
-})
+});
