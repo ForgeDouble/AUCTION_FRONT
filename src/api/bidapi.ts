e7e5864c99b6f09dd1b453977d181d1dd93a4ea3
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-09-18 23:59:16 +0900

    Feat: product 호출 fetch 구현

--- src/api/bidapi.ts
+++ src/api/bidapi.ts
@@ -1,4 +1,7 @@
-import type { BidLogDto } from "../pages/auction_detail/AuctionDetailDto";
+import type {
+  BidLogDto,
+  ProductDto,
+} from "../pages/auction_detail/AuctionDetailDto";
 import type { ApiResponse } from "../type/CommonType";
 
 export const fetchBids = async (
@@ -10,6 +13,24 @@ export const fetchBids = async (
       "Content-Type": "application/json",
     },
     credentials: "include", // 필요하면 쿠키 포함
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch bids: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const fetchProductById = async (
+  productId: number
+): Promise<ApiResponse<ProductDto>> => {
+  const response = await fetch(`http://localhost:8080/prodcut/${productId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
   });
 
   if (!response.ok) {
