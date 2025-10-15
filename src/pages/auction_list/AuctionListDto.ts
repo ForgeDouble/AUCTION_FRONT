674d4b1dd81ea2b274b48e3f1cf35d147b432a15
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-10-15 23:23:27 +0900

    Feat: category fetch 구현, 백엔드 데이터로 ui 구현
    - category fetch, dto 구현
    - 백엔드 데이터 기반 Ui 변경

--- src/pages/auction_list/AuctionListDto.ts
+++ src/pages/auction_list/AuctionListDto.ts
@@ -1,8 +1,19 @@
+export type categoryDto = {
+  categoryId: number;
+  categoryName: string;
+};
+
 export interface ProductListDto {
   productId: number;
-  categoryId: number;
+  path: categoryDto[];
   productName: string;
   productContent: string;
   price: number;
   sellYN: string;
+}
+
+export interface ParentCategoriesDto {
+  categoryId: number;
+  categoryName: string;
+  parentId: null | string;
 }
