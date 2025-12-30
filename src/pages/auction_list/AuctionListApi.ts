201b2e0a3cebde7b7e09055395d8d273e6c440e2
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-12-30 23:43:10 +0900

    Fix: wishlist fetch 함수 이동
    - 공용 api로 이동

--- src/pages/auction_list/AuctionListApi.ts
+++ src/pages/auction_list/AuctionListApi.ts
@@ -44,49 +44,6 @@ export const fetchParentCategories = async (): Promise<
   return response.json();
 };
 
-export const fetchCreateWishlist = async (
-  token: string | null,
-  prodcutId: number
-): Promise<ApiResponse<null>> => {
-  const formData = new FormData();
-  formData.append("productId", prodcutId.toString());
-
-  const response = await fetch(`http://localhost:8080/wishlist/create`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: formData,
-  });
-
-  if (!response.ok) {
-    throw new Error(`Failed to fetch bids: ${response.status}`);
-  }
-
-  return response.json();
-};
-
-export const fetchDeleteWishlist = async (
-  token: string | null,
-  wishlistId: number
-): Promise<ApiResponse<null>> => {
-  const response = await fetch(
-    `http://localhost:8080/wishlist/delete/${wishlistId}`,
-    {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error(`Failed to fetch bids: ${response.status}`);
-  }
-
-  return response.json();
-};
-
 export const fetchWishlistByUser = async (
   token: string | null
 ): Promise<ApiResponse<wishlistDto[]>> => {
