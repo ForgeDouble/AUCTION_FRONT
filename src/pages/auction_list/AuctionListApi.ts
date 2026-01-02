8e22f1432c00b04b413802813fe37d5efb10bb73
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-01-02 13:44:25 +0900

    Fix: fetch함수 엔드포인트 이름 변경
    - with_children -> with_children_and_count

--- src/pages/auction_list/AuctionListApi.ts
+++ src/pages/auction_list/AuctionListApi.ts
@@ -29,13 +29,16 @@ export const fetchProducts = async (
 export const fetchParentCategories = async (): Promise<
   ApiResponse<ParentCategoriesDto[]>
 > => {
-  const response = await fetch(`http://localhost:8080/category/with_children`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include", // 필요하면 쿠키 포함
-  });
+  const response = await fetch(
+    `http://localhost:8080/category/with_children_and_count`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // 필요하면 쿠키 포함
+    }
+  );
 
   if (!response.ok) {
     throw new Error(`Failed to fetch bids: ${response.status}`);
