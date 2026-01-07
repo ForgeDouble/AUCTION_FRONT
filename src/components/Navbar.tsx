8833f6290e878514ed3a32838e61fb3dc598951c
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-01-07 23:55:50 +0900

    Fix: 로고 크기 조정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 src={logoBid}
                 alt="BID"
                 style={{
-                  height: "70px",
+                  height: "65px",
                   display: "block",
                   float: "left",
                   filter: "brightness(2.0)",
