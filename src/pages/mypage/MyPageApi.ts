c42d49d94760a17aca6d4c518d43a8f8f168679d
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-01-09 17:23:28 +0900

    Fix: 나의 경매 목록 페이징 처리 추가
    - fetch 페이징 처리
    - 페이징 컴포넌트 사용
    - 페이징 state 추가

--- src/pages/mypage/MyPageApi.ts
+++ src/pages/mypage/MyPageApi.ts
@@ -52,15 +52,28 @@ export const fetchProductsByUser = async (
 };
 
 export const fetchBidsByUser = async (
-  token: string | null
-): Promise<ApiResponse<BidListDto[]>> => {
-  const response = await fetch(`http://localhost:8080/bid/allByUser`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  });
+  token: string | null,
+  page: number = 0,
+  size: number = 10
+): Promise<
+  ApiResponse<{
+    content: BidListDto[];
+    totalElements: number;
+    totalPages: number;
+    currentPage: number;
+    size: number;
+  }>
+> => {
+  const response = await fetch(
+    `http://localhost:8080/bid/allByUser?page=${page}&size=${size}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   if (!response.ok) {
     throw new Error(`Failed to fetch bids: ${response.status}`);
