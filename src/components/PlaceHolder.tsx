fe0805e2e33fa0cb8a27b97af68a85a76cda7ef2
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-01-12 20:23:32 +0900

    Fix: placeHolder 크기 조정

--- src/components/PlaceHolder.tsx
+++ src/components/PlaceHolder.tsx
@@ -1,7 +1,7 @@
 const PlaceHolder = () => {
   return (
     <>
-      <div className="w-full h-48 bg-gradient-to-br from-purple-900/30 to-pink-900/30 flex items-center justify-center">
+      <div className="w-full h-40 bg-gradient-to-br from-purple-900/30 to-pink-900/30 flex items-center justify-center">
         <span className="text-white/50">No Image</span>
       </div>
     </>
