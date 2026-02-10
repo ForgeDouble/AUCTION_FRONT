785cc52230ede948056ce9e73111920d8cb888b0
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-02-10 09:59:34 +0900

    Feat: Errors - DataReadError 추가
    - 500에러 이지만 모달창이 아니라 페이지 자체를 렌더링 시킬 때 사용

--- src/errors/Errors.ts
+++ src/errors/Errors.ts
@@ -23,10 +23,22 @@ export class UnauthorizedError extends ApiError {
   constructor(additionalInfo?: string) {
     super(
       401, // httpStatus
-      "NOT_ALLOWED", // statusCode (도메인 코드)
+      "INVALID_TOKEN", // statusCode (도메인 코드)
       "로그인이 필요합니다.", // message
       additionalInfo, // optional
     );
     this.name = "UnauthorizedError";
+  }
+}
+
+export class DataReadError extends ApiError {
+  constructor(additionalInfo?: string) {
+    super(
+      500, // httpStatus
+      "INTERNAL_SERVER_ERROR", // statusCode (도메인 코드)
+      "서버 내부에서 오류가 발생했습니다. 관리자에게 문의해주세요.", // message
+      additionalInfo, // optional
+    );
+    this.name = "DataReadError";
   }
 }
