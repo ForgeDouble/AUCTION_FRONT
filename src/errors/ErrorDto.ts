b30c44bda240034f1f3420988cbc4afd672aaa16
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-02-10 09:59:52 +0900

    Feat: ErrorDto - 예외 사항 추가

--- src/errors/ErrorDto.ts
+++ src/errors/ErrorDto.ts
@@ -1,23 +1,31 @@
 export type ErrorCode =
+  /** 공용 */
   | "INTERNAL_SERVER_ERROR"
+  | "INTERNAL_ERROR"
   | "INVALID_TOKEN"
+  | "INVALID_USER"
+  | "DATA_NOT_FOUND"
+  /** 이메일 전송 */
   | "INVALID_PS_TOKEN"
-  | "NOT_ALLOWED"
-  | "USER_NOT_FOUND"
-  | "PRODUCT_NOT_FOUND"
-  | "SELLER_NOT_ALLOWED"
-  | "QUANTITY_ERROR"
-  | "LOW_PRICE"
+  /** 위시리스트 */
+  | "SELF_WISHLIST_FORBIDDEN"
+  /** 입찰 */
   | "INVALID_INPUT_FORMAT"
   | "BID_VALIDATION_ERROR"
-  | "INTERNAL_ERROR"
-  | "FAILED_TO_SEND_EMAIL";
+  | "NOT_PROCESSING"
+  | "SELLER_NOT_ALLOWED"
+  | "QUANTITY_ERROR"
+  | "LOW_PRICE";
 
 export type ErrorHandlingResult =
   | { type: "REDIRECT"; to: string }
   | { type: "TOAST"; message: string }
   | { type: "MODAL"; message: string }
+  | { type: "ERROR"; message: string }
+  | { type: "WARNING"; message: string }
   | { type: "DIALOG"; message: string }
   | { type: "FIELD_ERROR"; field: string; message: string }
   | { type: "IGNORE" }
   | { type: "AUTH"; message: string };
+
+export type ErrorStatus = "404" | "500" | "network";
