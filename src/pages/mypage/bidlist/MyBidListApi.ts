0d41f137fa60046a82f89a53c571952dd3ffb505
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-03-04 20:27:20 +0900

    Feat: MyBidlist - 입찰 내역 조회 api 수정

--- src/pages/mypage/bidlist/MyBidListApi.ts
+++ src/pages/mypage/bidlist/MyBidListApi.ts
@@ -11,7 +11,8 @@ export const fetchBidsByUser = async (
   token: string | null,
   page: number = 0,
   size: number = 10,
-  status: Status | null,
+  statuses: Status[] | null,
+  isWinned: "Y" | "N" | null = null,
 ): Promise<
   ApiResponse<{
     content: BidListDto[];
@@ -22,17 +23,23 @@ export const fetchBidsByUser = async (
     size: number;
   }>
 > => {
-  const response = await fetch(
-    `${BASE}/bid/allByUser?page=${page}&size=${size}&status=${status}`,
-    {
-      method: "GET",
+  const params = new URLSearchParams({
+    page: page.toString(),
+    size: size.toString(),
+  });
 
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
+  if (statuses && statuses.length > 0) {
+    statuses.forEach((s) => params.append("statuses", s)); // 여러 개 append
+  }
+  if (isWinned) params.append("isWinned", isWinned);
+  const response = await fetch(`${BASE}/bid/allByUser?${params}`, {
+    method: "GET",
+
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
     },
-  );
+  });
 
   if (!response.ok) {
     const body = await response.json();
